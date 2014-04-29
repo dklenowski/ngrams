618b80a589d6c49e0d2d0c9681bbd38a18fa1bdf
davek <dklenowski@gmail.com>
2014-04-29 17:21:17 +1000

    fixed whitespace.

--- src/main/java/com/orbious/google/ngrams/RedisSet.java
+++ src/main/java/com/orbious/google/ngrams/RedisSet.java
@@ -9,37 +9,37 @@ import redis.clients.jedis.exceptions.JedisException;
 public class RedisSet {
 
   private JedisPool pool;
-  
+
   private String hostip;
   private int hostport;
   private String setname;
-  
+
   protected static final Logger logger = Logger.getLogger(Config.logrealm);
-  
+
   public RedisSet(String hostip, int hostport, String setname) { 
     this.hostip = hostip;
     this.hostport = hostport;
     this.setname = setname;
   }
-  
+
   public synchronized void connect() throws RedisSetException {
     if ( pool != null ) return;
-    
+
     logger.info("connecting to redis on " + hostip + ": " + hostport);
-    
+
     JedisPoolConfig config = new JedisPoolConfig();
     config.setTestOnBorrow(true);
     pool = new JedisPool(config, hostip, hostport, Config.redistimeout);
 
     if ( !isConnected() ) 
-    	throw new RedisSetException("Failed to connect to redis at " + hostip + ": " +hostport);
+      throw new RedisSetException("Failed to connect to redis at " + hostip + ": " +hostport);
   }
-  
+
   public boolean isConnected() {
     Jedis jedis = pool.getResource();
     boolean connected = jedis.isConnected();
     pool.returnResource(jedis);
-    
+
     return connected;
   }
 
@@ -49,26 +49,26 @@ public class RedisSet {
     } catch ( JedisException je ) { 
       logger.warn("failed to destroy jedis pool");
     } finally {
-    	pool = null;
+      pool = null;
     }
   }
-  
+
   public synchronized boolean contains(String key) {
-  	logger.debug("searching for key " + key);
+    logger.debug("searching for key " + key);
     Jedis jedis = pool.getResource();
     boolean ismember = jedis.sismember(setname, key);
     pool.returnResource(jedis);
-    
+
     return ismember;
   }
-  
+
   public synchronized void put(String key) {
-  	logger.debug("adding " + key + " to set " + setname);
+    logger.debug("adding " + key + " to set " + setname);
     Jedis jedis = pool.getResource();
     jedis.sadd(setname, key);
     pool.returnResource(jedis);
   }
-  
+
   public synchronized String connectStr() {
     return hostip + ": " + hostport + " (" + setname + ")";
   }

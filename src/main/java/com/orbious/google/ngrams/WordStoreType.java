2611ff0ab04f363c1620ea598bf9cdcd8d92f8c6
davek <dklenowski@ebay.com>
2015-01-11 18:25:18 +1100

    uncommented out some types.

--- src/main/java/com/orbious/google/ngrams/WordStoreType.java
+++ src/main/java/com/orbious/google/ngrams/WordStoreType.java
@@ -8,10 +8,10 @@ public enum WordStoreType {
   PRON, //pronouns
   DET,  //determiners and articles 
   CONJ, //conjunctions
-  ADP;  //prepositions and postpositions
-//  NUM,  //numerals
-//  PRT,  //particles
-//  X;    //catchall
+  ADP,  //prepositions and postpositions
+  NUM,  //numerals
+  PRT,  //particles
+  X;    //catchall
 
   public static String toString(WordStoreType type) {
     switch ( type ) {
@@ -31,12 +31,12 @@ public enum WordStoreType {
       return "conj";
     case ADP:
       return "adp";
-//    case NUM:
-//      return "num";
-//    case PRT:
-//      return "prt";
-//    case X:
-//      return "x";
+    case NUM:
+      return "num";
+    case PRT:
+      return "prt";
+    case X:
+      return "x";
     default:
       return null;
     }
@@ -60,12 +60,12 @@ public enum WordStoreType {
       return WordStoreType.CONJ;
     case "adp":
       return WordStoreType.ADP;
-//    case "num":
-//      return WordStoreType.NUM;
-//    case "prt":
-//      return WordStoreType.PRT;
-//    case "x":
-//      return WordStoreType.X;
+    case "num":
+      return WordStoreType.NUM;
+    case "prt":
+      return WordStoreType.PRT;
+    case "x":
+      return WordStoreType.X;
     default:
       return null;
     }
